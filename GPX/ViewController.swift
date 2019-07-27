a96b686d1a5f08c4078c0c3643d25937d17b54a4
Adam Tootle <adamtootle@gmail.com>
2019-07-26 20:01:46 -0400

    Initial Commit

---
+++ GPX/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GPX
+//
+//  Created by Adam Tootle on 7/26/19.
+//  Copyright © 2019 Adam Tootle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
